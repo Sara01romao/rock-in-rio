7b627c5d0939f0ec5557069bb2c817a1dde8cc30
Sara Romão <sara-romao@live.com>
2021-08-29 11:28:06 -0300

    função de login com objetos

--- js/main.js
+++ js/main.js
@@ -4,18 +4,39 @@
   var senhas =123456;
   var users = "teste@teste.com";
 
+ const userData =[
+   {
+     name:"sara",
+     login:"teste@teste.com",
+     senha:123
+   },
 
+   {
+    name:"Gome",
+    login:"teste@teste.com",
+    senha:1234
+  }
 
-  function entrar(){
-      if(senhas==senha.value && users == login.value){
-          console.log('ok')
-          window.location.href = "http://127.0.0.1:5500/pages/home.html";
-      }else{
-        document.getElementById("msg").innerHTML = "senha errada";
+ ]
 
-      }
-        
+//verificar usuario
+ function verifica(){
 
-}
+  const result = userData.find(u => u.senha == senha.value && u.login == login.value)
   
+
+  if(!result){
   
+    document.getElementById("msg").innerHTML = "senha ou email inválido";
+  }else{
+    window.location.assign("http://127.0.0.1:5500/pages/home.html");
+  
+  }
+  
+ }
+
+  
+
+
+
+/************************************************************************ */
